618a4c199dc5767244cc0e1006ea719aac9eb3c3
Dong Hao <haodong.donh@gmail.com>
2016-07-15 00:17:26 +0800

    Accepted

--- Media_of_Two_Sorted_Arrays/findMedianSortedArrays.c
+++ Media_of_Two_Sorted_Arrays/findMedianSortedArrays.c
@@ -16,25 +16,26 @@ double findK(int *a, int a_size, int *b, int b_size, int k)
     if (a_size > b_size) {
         return findK(b, b_size, a, a_size, k);
     }
-    
-    /*So the problem turns out to be finding the kth smallest number*/
+
     if (a_size == 0) {
         /*Since a is empty, just regard b[k-1] as the kth smallest number */
         return b[k-1];
     }
-
-    /*in case that a has a small length*/
-    int i_a = MIN(a_size, k/2);
-    int i_b = k - i_a;
+    
     if (k == 1) {
         return MIN(a[0], b[0]);
     }
-    if (a[i_a-1] < b[i_b-1]) {
-        return findK(a+i_a, a_size-i_a, b, b_size, k-i_a);
-    } else if (a[i_a-1] > b[i_b-1]) {
-        return findK(a, a_size, b, b_size-i_b, k-i_b);
+    /*So the problem turns out to be finding the kth smallest number*/
+
+    /*in case that a has a small length*/
+    int a_mid = MIN(a_size, k/2);
+    int b_mid = k - a_mid;
+    if (a[a_mid-1] < b[b_mid-1]) {
+        return findK(a+a_mid, a_size-a_mid, b, b_size, k-a_mid);
+    } else if (a[a_mid-1] > b[b_mid-1]) {
+        return findK(a, a_size, b+b_mid, b_size-b_mid, k-b_mid);
     } else {
-        return a[i_a-1];
+        return a[a_mid-1];
     }
 }
 
@@ -47,15 +48,15 @@ double findMedianSortedArrays(int *nums1, int nums1Size, int *nums2, int nums2Si
         ret = (double)findK(nums1, nums1Size, nums2, nums2Size, mid+1);
     else
         ret = (double)(findK(nums1, nums1Size, nums2, nums2Size, mid) + 
-                findK(nums1, nums1Size, nums2, nums2Size, mid)) / 2;
+                findK(nums1, nums1Size, nums2, nums2Size, mid+1)) / 2;
     return ret;
 }
 
 
 int main()
 {
-    int a[2] = {1,3};
-    int b[1] = {2};
+    int a[] = {1,2};
+    int b[] = {3,4};
 
     int a_size = sizeof(a)/sizeof(a[0]);
     int b_size = sizeof(b)/sizeof(b[0]);
