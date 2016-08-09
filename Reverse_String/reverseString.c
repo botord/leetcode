8103eb5b261b19b702ec6d431de84d5d290ec021
Dong Hao <haodong.donh@gmail.com>
2016-08-09 21:17:44 +0800

    reverse string accepted

--- Reverse_String/reverseString.c
+++ Reverse_String/reverseString.c
@@ -8,40 +8,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 char *reverseString(char *src) 
 {
     printf("src = %s\n", src);
-    int i = 0;
-    char *p = src;
-    printf("p = %s\n", p);
-
-    while (*p != '\0') {
-        p++;
-        i++;
+    int i, j;
+    i = strlen(src);
+    int len = i/2;
+    char tmp;
+    
+    //swap
+    for (j = 0; j < len; j++,i--) {
+        tmp = src[j];
+        src[j] = src[i-1];
+        src[i-1] = tmp;
     }
 
-    char *q = malloc(i * sizeof(char));
-    char *des = q;
-
-    while (i>0) {
-        i--;
-        p--;
-        printf("p = %c\n", *p);
-        *q = *p;
-        printf("q = %c\n", *q);
-        q++;
-    }
-    q++;
-    *q = '\0';
-
-    return des;
+    return src;
 }
+
 
 int main()
 {
-    char *s = "hello world!";
+    char s[] = "A man \
+                a plan \
+                a cameo \
+                Zena";
     char *d = reverseString(s);
 
     printf("d = %s\n", d);
+
+    return 0;
 }
