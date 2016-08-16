732e6ddaf3255958e857daf02caf756893cc9479
Dong Hao <haodong.donh@gmail.com>
2016-08-16 11:05:49 +0800

    nim game

---
+++ Nim_Game/canWinNim.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: canWinNim.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年08月16日 星期二 09时39分56秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool canWinNim (int n) {
+
+}
